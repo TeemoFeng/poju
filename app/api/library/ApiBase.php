0dd4afd1a1a3a8db2118ac881e92a00287b83d82
yanweifeng <yanweifeng@uroaming.com.cn>
2020-02-15 21:45:12 +0800

    解决cookie问题2

--- app/api/library/ApiBase.php
+++ app/api/library/ApiBase.php
@@ -75,6 +75,11 @@ class ApiBase
      */
     public function __construct(Request $request = null)
     {
+        //header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: http://dist.morketing.com");
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+        header('Access-Control-Allow-Headers:Origin,Content-Type,Accept,Token,X-Requested-With,device');
+        header("Access-Control-Allow-Credentials: true");
         $this->request = is_null($request) ? Request::instance() : $request;
         $this->db_app = Db::connect('database_morketing');
         // 控制器初始化
