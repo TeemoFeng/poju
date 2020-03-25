28c4e75151e51d3b79832730ccef37e44e3db944
yanweifeng <yanweifeng@uroaming.com.cn>
2020-03-25 21:48:12 +0800

    加入导出功能

--- app/backstage/controller/Enroll.php
+++ app/backstage/controller/Enroll.php
@@ -10,13 +10,45 @@ use app\common\controller\Base;
 use app\backstage\model\SummitEnroll;
 use think\Config;
 use think\Db;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
 
 class Enroll extends Base
 {
     public function items()
     {
+        $summit_name = $this->request->param('summit_name', '');
+        $sd = $this->request->param('sd');
+        $ed = $this->request->param('ed');
+        $this->assign('sd', $sd);
+        $this->assign('ed', $ed);
+        $start_date = !empty($sd) ? strtotime($sd) : 0;
+        $end_date = !empty($ed) ? strtotime($ed) : 0;
+        $this->assign('summit_name', $summit_name);
+        $where2 = [];
+
+        if (!empty($start_date) && !empty($end_date)) {
+            if ($start_date > $end_date) {
+                $this->error('开始时间不能大于结束时间');
+            }
+            $where2['sub_time'] = ['between', $start_date.','.$end_date];
+        } else if (!empty($start_date) && empty($end_date)) {
+            $where2['sub_time'] = ['>', $start_date];
+        } else if (empty($start_date) && !empty($end_date)) {
+            $where2['sub_time'] = ['<', $end_date];
+        }
         $categoryModel = new \app\backstage\model\Category();
-        $list = SummitEnroll::order('id ASC')->paginate(20)->each(function ($v) use($categoryModel) {
+        if (!empty($summit_name)) {
+            $ids = $categoryModel->where('name',"like","%".$summit_name."%")->column('id');
+            if (!empty($ids)) {
+                $where2['cid'] = ['in', $ids];
+            }
+        }
+        $list = SummitEnroll::where($where2)->order('id ASC')->paginate(20, false ,['query'=>request()->param()])->each(function ($v) use($categoryModel) {
             $v['summit_name'] = $categoryModel::where(['id' => $v['cid']])->value('name');
             $v['sub_time'] = date('Y-m-d H:i:s', $v['sub_time']);
         });
@@ -63,6 +95,67 @@ class Enroll extends Base
         $idlist=$this->request->param("idlist");
         $n = $summitBanner->where("id","in",$idlist)->delete();
         return json($n>0?["code"=>1,"msg"=>"所选记录已删除"]:["code"=>2,"msg"=>"当前没有记录被删除"]);
+    }
+
+
+    public function getExcel()
+    {
+        $chat = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'];
+        $summit_name = $this->request->param('summit_name', '');
+        $sd = $this->request->param('sd', 0);
+        $ed = $this->request->param('ed', 0);
+        $this->assign('sd', $sd);
+        $this->assign('ed', $ed);
+        $start_date = !empty($sd) ? strtotime($sd) : 0;
+        $end_date = !empty($ed) ? strtotime($ed) : 0;
+        $this->assign('summit_name', $summit_name);
+        $where2 = [];
+
+        if (!empty($start_date) && !empty($end_date)) {
+            if ($start_date > $end_date) {
+                $this->error('开始时间不能大于结束时间');
+            }
+            $where2['sub_time'] = ['between', $start_date.','.$end_date];
+        } else if (!empty($start_date) && empty($end_date)) {
+            $where2['sub_time'] = ['>', $start_date];
+        } else if (empty($start_date) && !empty($end_date)) {
+            $where2['sub_time'] = ['<', $end_date];
+        }
+        $categoryModel = new \app\backstage\model\Category();
+        if (!empty($summit_name)) {
+            $ids = $categoryModel->where('name',"like","%".$summit_name."%")->column('id');
+            if (!empty($ids)) {
+                $where2['cid'] = ['in', $ids];
+            }
+        }
+
+        $data = SummitEnroll::where($where2)->order('id ASC')->select();
+
+        $this->db_app = Db::connect('database_morketing');
+        $th = $this->db_app->table('diy_form')->select();
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setTitle("会议报名");
+        $startRowIndex = 1;
+
+        foreach($th as $index => $t){
+            $sheet->setCellValue($chat[$index].$startRowIndex, $t['label']);
+        }
+        foreach ($data as $index=>$item){
+            $startRowIndex +=1;
+
+            foreach ($th as $key=>$vo){
+                $sheet->setCellValue($chat[$key].$startRowIndex, $item[$vo['name']]);
+            }
+
+        }
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'.date('Ymd',time()).'会议报名'.'.xlsx"');
+        header('Cache-Control: max-age=0');
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('php://output');
+        exit;
     }
 }
 
